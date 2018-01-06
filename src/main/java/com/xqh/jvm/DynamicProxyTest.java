56a67fd56853a06346d9f4f725bf78565cb00d23
xuqianghui <merlinkael@gmail.com>
2018-01-06 17:14:24 +0800

    实现代码远程调用功能 示例

---
+++ src/main/java/com/xqh/jvm/DynamicProxyTest.java
@@ -0,0 +1,46 @@
+package com.xqh.jvm;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by leo on 2018/1/6.
+ */
+public class DynamicProxyTest {
+
+    interface IHello{
+        void sayHello();
+    }
+
+    static class Hello implements IHello{
+        @Override
+        public void sayHello() {
+            System.out.println("hello world");
+        }
+    }
+
+    static class DynamicProxy implements InvocationHandler{
+
+        Object originalObj;
+
+        Object bind(Object originalObj){
+            this.originalObj=originalObj;
+            return Proxy.newProxyInstance(originalObj.getClass().getClassLoader(),
+                    originalObj.getClass().getInterfaces(), this);
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("welcome");
+            return method.invoke(originalObj,args);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+        IHello iHello= (IHello) new DynamicProxy().bind(new Hello());
+        iHello.sayHello();
+    }
+
+}
